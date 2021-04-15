611a378162b7ff55d3a0e95958f626754f753f5e
qiulijian <qiulijian>
2021-04-15 10:07:28 +0800

    init

---
+++ Example/podTest/person.swift
@@ -0,0 +1,21 @@
+//
+//  person.swift
+//  podTest_Example
+//
+//  Created by qiulj on 2021/4/14.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+open class person{
+
+    open var i = 1
+    public init() {
+        
+    }
+    
+    open func teach1(){
+        print("podTest swift person teach1")
+    }
+}
---
+++ test.swift
@@ -0,0 +1,15 @@
+//
+//  test.swift
+//  Pods-podTest_Example
+//
+//  Created by qiulj on 2021/4/15.
+//
+
+import Foundation
+
+open class test{
+    
+    open func test1(){
+        print("test test1")
+    }
+}
